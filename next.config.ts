e075fc53e46a62f34721e1f91fee1ff969ff989c
Ravi Kiran Srikakaolapu <ravikiransrikakaolapu@Ravis-MacBook-Air.local>
2025-03-23 10:47:56 +0530

    firebase updated build command

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
-  output: 'export'
+  output: 'export',
+  images: {
+    unoptimized: true, // Required for static export if using images
+  },
 };
 
 export default nextConfig;
